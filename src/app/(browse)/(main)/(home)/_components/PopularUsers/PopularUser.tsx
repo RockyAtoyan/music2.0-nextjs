dd22960f95b1d8d9b1a00e15fbcd23518e294677
Rocky <atoyanrobert21@gmail.com>
2024-06-29 17:08:15 +0300

    change PopularUser theme

--- src/app/(browse)/(main)/(home)/_components/PopularUsers/PopularUser.tsx
+++ src/app/(browse)/(main)/(home)/_components/PopularUsers/PopularUser.tsx
@@ -10,16 +10,16 @@ export function PopularUser({ user }: Props) {
   return (
     <Link
       href={`/profile/${user.id}`}
-      className="flex items-center gap-6 transition-colors hover:bg-primary hover:text-background  p-8"
+      className="flex items-center gap-6 transition-colors hover:bg-primary hover:text-background  p-3"
     >
       <Image
         src={user.image || "/user.webp"}
         alt="user"
         width={500}
         height={500}
-        className="w-[70px] aspect-square rounded-full"
+        className="w-[50px] aspect-square rounded-full"
       />
-      <h3 className="font-semibold text-2xl">{user.login}</h3>
+      <h3 className="font-semibold text-xl">{user.login}</h3>
     </Link>
   );
 }
