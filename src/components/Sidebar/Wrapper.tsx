a2d1cd967fa0fb3e2a20214d477025a04c560d74
Rocky <atoyanrobert21@gmail.com>
2024-07-03 18:08:20 +0300

    fix sidebar recommended

--- src/components/Sidebar/Wrapper.tsx
+++ src/components/Sidebar/Wrapper.tsx
@@ -23,7 +23,7 @@ export function Wrapper({ recommendedUsers, subscribes, profile }: Props) {
     <div
       className={cn(
         "relative flex flex-col gap-6 h-full border-r-[1px] border-b-border w-[250px] transition-all",
-        collapse && "w-[100px]"
+        collapse && "w-[100px]",
       )}
     >
       <Button
@@ -33,7 +33,7 @@ export function Wrapper({ recommendedUsers, subscribes, profile }: Props) {
         className={cn(
           "absolute right-3 bottom-3",
           collapse && "right-1/2 translate-x-1/2",
-          currentSong && !collapse && "right-auto left-3"
+          currentSong && !collapse && "right-auto left-3",
         )}
       >
         {collapse ? <ChevronsRight /> : <ChevronsLeft />}
@@ -41,12 +41,12 @@ export function Wrapper({ recommendedUsers, subscribes, profile }: Props) {
       <Navbar user={profile} collapse={collapse} />
       {(!profile || (profile && !collapse)) && (
         <div className="flex flex-col items-center gap-2 transition-all">
-          {recommendedUsers?.length && (
+          {!!recommendedUsers?.length && (
             <div className="flex flex-col gap-4 w-full p-4 pt-0 pb-0 mb-0">
               <h2
                 className={cn(
                   "hidden lg:block text-sm text-zinc-400 font-semibold",
-                  collapse && "text-center"
+                  collapse && "text-center",
                 )}
               >
                 {collapse ? "Recs" : "Recommended"}
