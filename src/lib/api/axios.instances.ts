6ac9e5f7abb54ba7d3649697b75cd8a235864ec4
Rocky <atoyanrobert21@gmail.com>
2024-07-03 18:03:37 +0300

    fix api url

--- src/lib/api/axios.instances.ts
+++ src/lib/api/axios.instances.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const mainAxios = axios.create({
-  baseURL: `http://${process.env.NEXT_PUBLIC_API_URL || "localhost:5001"}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001"}`,
   withCredentials: true,
 });
 
