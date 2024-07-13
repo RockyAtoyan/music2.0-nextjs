65a916f8d19ff7c50ab61b59e1e41f88188773cd
Rocky <atoyanrobert21@gmail.com>
2024-07-13 14:36:53 +0300

    add error.tsx

---
+++ src/app/(browse)/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className={"h-screen flex items-center justify-center"}>
+      <h2 className={"font-semibold text-lg"}>Something went wrong!</h2>
+      <Button size={"lg"} onClick={() => reset()}>
+        Try again
+      </Button>
+    </div>
+  );
+}
