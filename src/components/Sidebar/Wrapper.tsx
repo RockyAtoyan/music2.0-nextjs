d5767241836366e5d95078b2f34ac32aed0901b8
Robert <atoyanrobert21@gmail.com>
2025-01-15 13:43:58 +0300

    fix: adaptive bugs

--- src/components/Sidebar/Wrapper.tsx
+++ src/components/Sidebar/Wrapper.tsx
@@ -35,7 +35,7 @@ export function Wrapper({ recommendedUsers, subscribes, profile }: Props) {
 					'absolute right-3 bottom-3 bg-background mb:max-lg:rounded-full',
 					collapse && 'right-1/2 translate-x-1/2',
 					!collapse &&
-						'mb:max-lg:-right-[3%] mb:max-md:-right-[7%] mb:max-lg:top-[0rem] mb:max-lg:rounded-tl-none',
+						'mb:max-lg:left-auto mb:max-lg:-right-[3%] mb:max-md:-right-[7%] mb:max-lg:top-[0rem] mb:max-lg:rounded-tl-none',
 					collapse && 'mb:max-lg:!right-6 mb:max-lg:!bottom-3',
 					currentSong && !collapse && 'right-auto left-3'
 				)}
@@ -53,7 +53,7 @@ export function Wrapper({ recommendedUsers, subscribes, profile }: Props) {
 			</Button>
 			<Navbar user={profile} collapse={collapse} setCollapse={setCollapse} />
 			{(!profile || (profile && !collapse)) && (
-				<div className='flex flex-col items-center gap-2 transition-all'>
+				<div className='flex flex-col items-center gap-2 transition-all mb:max-lg:hidden'>
 					{!!recommendedUsers?.length && (
 						<div className='flex flex-col gap-4 w-full p-4 pt-0 pb-0 mb-0'>
 							<h2
