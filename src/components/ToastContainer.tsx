19f913dcc90a48a1782e2d88d65760461f2f9340
Robert <atoyanrobert21@gmail.com>
2025-02-04 15:09:43 +0300

    fix: websockets

--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -1,37 +1,40 @@
-"use client";
+'use client'
 
-import { FC, useEffect, useState } from "react";
-import { toast } from "sonner";
-import { redirectToPage, updatePaths } from "@/actions/auth.actions";
-import { IUser } from "@/lib/types/IUser";
-import { io } from "socket.io-client";
+import { updatePaths } from '@/actions/auth.actions'
+import { IUser } from '@/lib/types/IUser'
+import { FC, useEffect } from 'react'
+import { io } from 'socket.io-client'
+import { toast } from 'sonner'
 
 interface Props {
-  profile: IUser | null;
+	profile: IUser | null
 }
 
 export const socket = io(
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001",
-);
+	process.env.NEXT_PUBLIC_API_URL + '/ws' || 'http://localhost:5001',
+	{
+		// path: '/server/ws',
+	}
+)
 
 export const ToastContainer: FC<Props> = ({ profile }) => {
-  useEffect(() => {
-    socket.removeAllListeners("message");
-    if (profile && socket) {
-      socket.send({ type: "connection", id: profile.id });
-      socket.on("message", (data) => {
-        const msg = JSON.parse(data);
-        if (msg.type !== "connection") {
-          toast.info(msg.text);
-        } else {
-          console.log("Соединение установлено.");
-        }
-        updatePaths(["/"]);
-      });
-    }
-  }, [profile?.id]);
+	useEffect(() => {
+		socket.removeAllListeners('message')
+		if (profile && socket) {
+			socket.send({ type: 'connection', id: profile.id })
+			socket.on('message', data => {
+				const msg = JSON.parse(data)
+				if (msg.type !== 'connection') {
+					toast.info(msg.text)
+				} else {
+					console.log('Соединение установлено.')
+				}
+				updatePaths(['/'])
+			})
+		}
+	}, [profile?.id])
 
-  return null;
-};
+	return null
+}
 
-export default ToastContainer;
+export default ToastContainer
