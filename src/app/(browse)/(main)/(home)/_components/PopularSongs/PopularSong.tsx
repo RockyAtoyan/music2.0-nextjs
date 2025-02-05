511b40329f2a3cd2e601eda2ac8f60bd69d321f1
Robert <atoyanrobert21@gmail.com>
2025-02-05 15:21:12 +0300

    feat: first screen song donwload pending

--- src/app/(browse)/(main)/(home)/_components/PopularSongs/PopularSong.tsx
+++ src/app/(browse)/(main)/(home)/_components/PopularSongs/PopularSong.tsx
@@ -1,77 +1,81 @@
-"use client";
+'use client'
 
-import { PlayerButton } from "@/components/Player/PlayerButton";
-import { Button } from "@/components/ui/button";
-import { useAppDispatch } from "@/hooks/useAppDispatch";
-import { useAppSelector } from "@/hooks/useAppSelector";
-import { downloadAudio } from "@/lib/services/audio.service";
-import { ISong } from "@/lib/types/ISong";
-import { cn } from "@/lib/utils";
-import { Download } from "lucide-react";
-import Image from "next/image";
-import { useTransition } from "react";
-import { toast } from "sonner";
+import { PlayerButton } from '@/components/Player/PlayerButton'
+import { Button } from '@/components/ui/button'
+import { downloadAudio } from '@/lib/services/audio.service'
+import { ISong } from '@/lib/types/ISong'
+import { cn } from '@/lib/utils'
+import { Download } from 'lucide-react'
+import Image from 'next/image'
+import { useState, useTransition } from 'react'
+import { toast } from 'sonner'
 
 interface Props {
-  song: ISong;
-  playlist: ISong[];
+	song: ISong
+	playlist: ISong[]
 }
 
 export function PopularSong({ song, playlist }: Props) {
-  const [isPending, startTransition] = useTransition();
+	const [isPending, startTransition] = useTransition()
 
-  return (
-    <div
-      className={cn(
-        "select-none flex flex-col gap-4 rounded-xl bg-secondary p-2",
-      )}
-    >
-      <div className="p-[2px] bg-gradient-to-r from-teal-400 to-yellow-200 rounded-lg">
-        <Image
-          src={song.image || "/logo.png"}
-          alt={"user"}
-          width={500}
-          height={500}
-          className={cn(
-            "rounded-lg w-full aspect-square object-cover object-center",
-          )}
-        />
-      </div>
-      <div className="flex flex-col items-center gap-3">
-        <div className={cn("flex flex-col items-center gap-2 w-full")}>
-          <h3 className="text-lg font-semibold text-nowrap w-full overflow-hidden text-center px-3">
-            {song.title}
-          </h3>
-          <h4 className="text-sm text-zinc-500 font-semibold">{song.author}</h4>
-        </div>
-        <div className="flex items-center gap-2">
-          <PlayerButton song={song} playlist={playlist} />
-          {song.file && (
-            <Button
-              size={"icon"}
-              variant={"outline"}
-              disabled={isPending}
-              onClick={() => {
-                startTransition(() => {
-                  downloadAudio(song.file, song.title + ".mp3")
-                    .then((file) => {
-                      if (!file) {
-                        return toast.error("Error");
-                      }
-                      toast.success("Downloaded!");
-                    })
-                    .catch((reason) => {
-                      const e = reason as Error;
-                      toast.error(reason.message);
-                    });
-                });
-              }}
-            >
-              <Download />
-            </Button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	const [downloadPending, setDownloadPending] = useState(false)
+
+	return (
+		<div
+			className={cn(
+				'select-none flex flex-col gap-4 rounded-xl bg-secondary p-2'
+			)}
+		>
+			<div className='p-[2px] bg-gradient-to-r from-teal-400 to-yellow-200 rounded-lg'>
+				<Image
+					src={song.image || '/logo.png'}
+					alt={'user'}
+					width={500}
+					height={500}
+					className={cn(
+						'rounded-lg w-full aspect-square object-cover object-center'
+					)}
+				/>
+			</div>
+			<div className='flex flex-col items-center gap-3'>
+				<div className={cn('flex flex-col items-center gap-2 w-full')}>
+					<h3 className='text-lg font-semibold text-nowrap w-full overflow-hidden text-center px-3'>
+						{song.title}
+					</h3>
+					<h4 className='text-sm text-zinc-500 font-semibold'>{song.author}</h4>
+				</div>
+				<div className='flex items-center gap-2'>
+					<PlayerButton song={song} playlist={playlist} />
+					{song.file && (
+						<Button
+							size={'icon'}
+							variant={'outline'}
+							disabled={isPending}
+							onClick={() => {
+								setDownloadPending(true)
+								startTransition(() => {
+									downloadAudio(song.file, song.title + '.mp3')
+										.then(file => {
+											if (!file) {
+												return toast.error('Error')
+											}
+											toast.success('Downloaded!')
+										})
+										.catch(reason => {
+											const e = reason as Error
+											toast.error(reason.message)
+										})
+										.finally(() => {
+											setDownloadPending(false)
+										})
+								})
+							}}
+						>
+							<Download />
+						</Button>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
